9809ab01f007ae9da8a698d055c3805e94fc636c
Chester Leung <chester.leung@berkeley.edu>
2020-09-29 22:54:33 +0000

    Remove print statements

--- python-package/securexgboost/core.py
+++ python-package/securexgboost/core.py
@@ -2506,7 +2506,6 @@ def init_client(remote_addr=None, user_name=None, client_list=[],
     _CONF["remote_addr"] = remote_addr;
     _CONF["current_user"] = user_name
     _clients = set(sorted(client_list))
-    print(sorted(client_list))
     _clients.add(user_name)
     _CONF["client_list"] = list(_clients)
 
